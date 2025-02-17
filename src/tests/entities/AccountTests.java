46578f3d2c389258f91a3885247c88c729e1d379
Arnaldo Cardone <arnaldo.cardone@gmail.com>
2025-02-17 11:43:31 -0300

    Implementação de Factory de Accounts e novos testes

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -3,6 +3,7 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -11,9 +12,19 @@ public class AccountTests {
         double amount = 200.00;
         double expectedValue = 196.00;
 
-        Account account = new Account(1L, 0.0);
+        Account account = AccountFactory.createEmptyAccount();
         account.deposit(amount);
         //Verifica se o deposito descontou a taxa corretamente
+        Assertions.assertEquals(expectedValue, account.getBalance() );
+    }
+
+    @Test
+    public void depositShoudNotIncreaseBalanceWhenNegativeAmount() {
+        double amount = -200.00;
+        double expectedValue = 100.0;
+        Account account = AccountFactory.createAccount(expectedValue);
+        account.deposit(-200.00);
+        //Valida se o depoisto com valor negativo não foi efetuado, mantendo o saldo inicial
         Assertions.assertEquals(expectedValue, account.getBalance() );
     }
 }
